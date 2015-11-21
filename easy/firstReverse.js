a40656c1007e23b5765443e4e07ad076faea9096
Richard Felix <rfelixmail@gmail.com>
2015-11-20 23:03:56 -0500

    manual way to firstReverse

--- easy/firstReverse.js
+++ easy/firstReverse.js
@@ -1,5 +1,21 @@
 function FirstReverse(str) { 
 
-  return str.split("").reverse().join(""); 
-         
+    var tmp = [];
+    
+    for( var x = 0; x < str.length; x++ ){
+        tmp.push( str[(str.length-x)-1] );
+    }
+    
+    str = tmp.join("");
+    
+    return str;         
 }
+
+console.log(FirstReverse("ReverseTime"));
+
+/*********** Easy Way ***********************/
+//function FirstReverse(str) { 
+//
+//  return str.split("").reverse().join(""); 
+//         
+//}

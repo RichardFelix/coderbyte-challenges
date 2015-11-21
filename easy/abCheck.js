50304ee5f757619973976aeb4fd4c4c9f83e4e53
Richard Felix <rfelixmail@gmail.com>
2015-11-21 00:29:40 -0500

    fixed a variable in for loop for abCheck

--- easy/abCheck.js
+++ easy/abCheck.js
@@ -5,7 +5,7 @@ function ABCheck(str) {
     for( var x = 0; x < str.length; x++ ){
         if(str[x] == 'a' || str[x] == 'b'){
             for( var y = 0; y < 2; y++ ){
-                if(str[x+1] == 'b' || str[x+1] == 'a' ){
+                if(str[x+y] == 'b' || str[x+y] == 'a' ){
                     return false;
                 }
             }
